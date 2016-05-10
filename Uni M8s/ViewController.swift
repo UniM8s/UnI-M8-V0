de2d280568e7469f3d5edceaaeee7e67bcc2b2a7
LCMBAppDevelopers <Unim8s@gmail.com>
2016-05-10 21:51:06 +0100

    Initial Commit

---
+++ Uni M8s/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Uni M8s
+//
+//  Created by louis christodoulou on 03/05/2016.
+//  Copyright © 2016 Louis Loizou-Christodoulou & Marco Romeo Barbato Apps. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Uni M8sTests/Uni_M8sTests.swift
@@ -0,0 +1,36 @@
+//
+//  Uni_M8sTests.swift
+//  Uni M8sTests
+//
+//  Created by louis christodoulou on 03/05/2016.
+//  Copyright © 2016 Louis Loizou-Christodoulou & Marco Romeo Barbato Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Uni_M8s
+
+class Uni_M8sTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
